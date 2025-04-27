473f6a39bd3ef3be8911560b76d94afbafed8cf1
AdisaDuke <rasakadisa.fcaib@gmail.com>
2025-04-27 09:14:00 +0100

    form2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary"; // 👈 Import
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
