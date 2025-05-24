20d03a201be9d28cf4ef2b5c79a772b97f4869b0
AdisaDuke <rasakadisa.fcaib@gmail.com>
2025-05-24 20:29:28 +0100

    loading

---
+++ src/components/UI/SmartImage.js
@@ -0,0 +1,43 @@
+// SmartImage.js
+import React, { useState } from "react";
+
+function SmartImage({ src, webpSrc, alt, ...rest }) {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div style={{ position: "relative", width: "100%", height: "auto" }}>
+      {!loaded && (
+        <div
+          style={{
+            backgroundColor: "#ccc",
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            borderRadius: "8px",
+          }}
+        />
+      )}
+      <picture>
+        <source srcSet={webpSrc} type="image/webp" />
+        <img
+          src={src}
+          alt={alt}
+          onLoad={() => setLoaded(true)}
+          style={{
+            display: "block",
+            width: "100%",
+            height: "auto",
+            opacity: loaded ? 1 : 0,
+            transition: "opacity 0.5s ease-in",
+          }}
+          loading="eager"
+          {...rest}
+        />
+      </picture>
+    </div>
+  );
+}
+
+export default SmartImage;
